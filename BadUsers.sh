3b29449409c404dd2cc2ae89b173897a6b42dc5c
wextia <valcazar96@gmail.com>
2017-11-09 14:53:19 +0000

    Haber si me muero

--- BadUsers.sh
+++ BadUsers.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 p=0
-usage="Usage: BadUser.sh [-p]"
+t=0
+time=0
+usage="Usage: BadUser.sh [-p | -t time]"
 # detecció de opcions d'entrada: només son vàlids: sense paràmetres i -p
 if [ $# -ne 0 ]; then
     if  [ $# -eq 1 ]; then
@@ -9,10 +11,38 @@ if [ $# -ne 0 ]; then
         else
             echo $usage; exit 1
         fi 
+    fi
+    if  [ $# -eq 2 ]; then
+		if [ $1 == "-t" ]; then
+			t=1
+			time=$2
+			lastchr=`echo $time | rev | cut -c 1`
+			time=`echo $time | rev | cut -c 2- | rev`
+			if [ $lastchr == "m" ]; then
+				time=$(($time * 30))
+			fi
+		else
+			echo $usage; exit 1
+    	fi
     else 
         echo $usage; exit 1
     fi
 fi
+
+# tractar param -t
+if [ $t -eq 1 ]; then
+	lastlog=`last +$time | cut -d " " -f 1 | head -n -2`
+	for user in $lastlog; do
+		mod_files=`find / -user $user -mtime +$time`
+		if [ -z "$mod_files" ]; then
+			echo $user
+		fi
+	done
+
+	exit 0
+fi
+
+
 # afegiu una comanda per llegir el fitxer de password i només agafar el camp de # nom de l'usuari
 USERS=`cat /etc/passwd | cut -d : -f 1`
 for user in $USERS; do
@@ -24,9 +54,9 @@ for user in $USERS; do
     fi
     if [ $num_fich -eq 0 ] ; then
         if [ $p -eq 1 ]; then
-            # afegiu una comanda per detectar si l'usuari te processos en execució, 
-            # si no te ningú la variable $user_proc ha de ser 0
-            user_proc=`find $home -type f -user $user | wc -l`
+# afegiu una comanda per detectar si l'usuari te processos en execució, 
+# si no te ningú la variable $user_proc ha de ser 0
+			user_proc=`find $home -type f -user $user | wc -l`
             if [ $user_proc -eq 0 ]; then
                 echo "$user"
             fi
